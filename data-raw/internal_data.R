818a26537986de7d415ca5c366e851809321bb12
bgulbis <brian.gulbis@gmail.com>
2016-06-30 16:10:22 -0500

    medication class data

---
+++ data-raw/internal_data.R
@@ -0,0 +1,17 @@
+# internal_data.R
+#
+# data to be used internally in package
+
+# medication classes downloaded from EDW
+# last updated: 6/30/2016
+
+library(dplyr)
+
+BGTools::gzip_files("data-raw", pattern = "medication_classes.csv")
+
+med.classes <- readr::read_csv("data-raw/medication_classes.csv.gz") %>%
+    rename(med.class = `Drug Catalog`,
+           med.name = `Generic Drug Name`) %>%
+    distinct
+
+devtools::use_data(med.classes, internal = TRUE, overwrite = TRUE)

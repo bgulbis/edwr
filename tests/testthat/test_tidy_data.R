400739b345a64e42f75d9cff1aa9dc86898ebf85
bgulbis <brian.gulbis@gmail.com>
2016-06-30 14:24:51 -0500

    add test for default method

--- tests/testthat/test_tidy_data.R
+++ tests/testthat/test_tidy_data.R
@@ -8,4 +8,10 @@ context("Check tidy_data")
 test_that("returns proper class", {
     tmp <- read_edw_data(dir, "test_labs.csv", "labs")
     expect_s3_class(tidy_data(tmp), "labs")
+
+})
+
+test_that("returns default method warning", {
+    tmp <- read_edw_data(dir, "test_demographics.csv", "demographics")
+    expect_warning(tidy_data(tmp), "No tidy_data method available for class")
 })

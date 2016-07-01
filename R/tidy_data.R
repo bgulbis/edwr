c4d3eb5ff2ffc38266e53fca1344f124a7300c56
bgulbis <brian.gulbis@gmail.com>
2016-07-01 14:28:44 -0500

    use pipe for tidy meds_sched

--- R/tidy_data.R
+++ R/tidy_data.R
@@ -94,22 +94,17 @@ tidy_data.meds_cont <- function(x, ref, sched, ...) {
 #' @rdname tidy_data
 #' @importFrom magrittr %>%
 tidy_data.meds_sched <- function(x, ref, ...) {
-    # filter to tidy only scheduled meds
-    ref <- dplyr::filter_(ref, .dots = list(~group == "sched"))
-
     # for any med classes, lookup the meds included in the class
-    class.meds <- dplyr::filter_(ref, .dots = list(~type == "class"))
-    class.meds <- med_lookup(class.meds$name)
+    y <- dplyr::filter_(ref, .dots = list(~type == "class", ~group == "sched"))
+    class.meds <- med_lookup(y$name)
 
     # join the list of meds with any indivdual meds included
-    lookup.meds <- dplyr::filter_(ref, .dots = list(~type == "med"))
-    lookup.meds <- c(lookup.meds$name, class.meds$med.name)
+    y <- dplyr::filter_(ref, .dots = list(~type == "med", ~group == "sched"))
+    lookup.meds <- c(y$name, class.meds$med.name)
 
-    # filter to meds in lookup
-    tidy <- dplyr::filter_(x, .dots = list(~med %in% lookup.meds))
-
-    # sort by pie.id, med, med.datetime
-    tidy <- dplyr::arrange_(tidy, .dots = list("pie.id", "med", "med.datetime"))
+    # filter to keep only meds in lookup
+    tidy <- dplyr::filter_(x, .dots = list(~med %in% lookup.meds)) %>%
+        dplyr::arrange_(.dots = list("pie.id", "med", "med.datetime"))
 
     # keep original class
     class(tidy) <- class(x)

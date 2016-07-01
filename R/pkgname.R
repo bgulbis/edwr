15d8a01d852f6b5744543d0b779294d423026695
bgulbis <brian.gulbis@gmail.com>
2016-07-01 13:47:12 -0500

    add package documentation files

---
+++ R/pkgname.R
@@ -0,0 +1,13 @@
+#' Standardized Functions for Working with Data from the EDW
+#'
+#' \pkg{edwr} provides functions to facilitate working with data extracted from
+#' the EDW
+#'
+#' This package provides standardized functions for reading, tidying, and
+#' transforming data which was extracted from the Enterprise Data Warehouse
+#' (EDW) using the standardized EDW queries.
+#'
+#' The main functions include: \code{\link{read_edw_data}} for reading data from
+#' csv files, and \code{\link{tidy_data}} for transforming the data into a tidy
+#' data set.
+"_PACKAGE"

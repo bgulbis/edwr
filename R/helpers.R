a1228fbe8539c693786681deeaf53f7dfaffe0eb
bgulbis <brian.gulbis@gmail.com>
2016-07-02 17:52:56 -0500

    reformat code

--- R/helpers.R
+++ R/helpers.R
@@ -5,8 +5,6 @@ add_patients <- function(tidy, patients) {
     # make sure all patients are included in the data frame and the values for
     # added patients are FALSE
     tidy <- dplyr::full_join(tidy, patients["pie.id"], by = "pie.id") %>%
-        dplyr::mutate_at(
-            .cols = dplyr::vars(quote(-pie.id)),
-            .funs = function(x) dplyr::coalesce(x, FALSE)
-        )
+        dplyr::mutate_at(dplyr::vars(quote(-pie.id)),
+                         function(x) dplyr::coalesce(x, FALSE))
 }

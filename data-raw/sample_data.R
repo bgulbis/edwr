7f2169fff201cd5fb058d160410199e367071182
bgulbis <brian.gulbis@gmail.com>
2016-07-03 22:52:49 -0500

    add new sample data

--- data-raw/sample_data.R
+++ data-raw/sample_data.R
@@ -2,6 +2,12 @@
 
 library(edwr)
 
-labs <- read_data("data-raw", "test_labs.csv", "labs")
+diagnosis <- readRDS("data-raw/test_diag.Rds")
+labs <- readRDS("data-raw/test_labs.Rds")
+meds_cont <- readRDS("data-raw/test_meds_cont.Rds")
+meds_home <- readRDS("data-raw/test_meds_home.Rds")
+meds_sched <- readRDS("data-raw/test_meds_sched.Rds")
+warfarin <- readRDS("data-raw/test_warf.Rds")
 
-devtools::use_data(labs, overwrite = TRUE)
+devtools::use_data(diagnosis, labs, meds_cont, meds_home, meds_sched, warfarin,
+                   overwrite = TRUE)

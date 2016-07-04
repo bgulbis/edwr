4776f36e8afc6006593257a4a422df448489dcc2
bgulbis <brian.gulbis@gmail.com>
2016-07-04 07:58:34 -0500

    fix add_patients

--- R/helpers.R
+++ R/helpers.R
@@ -5,6 +5,7 @@ add_patients <- function(tidy, patients) {
     # make sure all patients are included in the data frame and the values for
     # added patients are FALSE
     tidy <- dplyr::full_join(tidy, patients["pie.id"], by = "pie.id") %>%
-        dplyr::mutate_at(dplyr::vars(quote(-pie.id)),
-                         function(x) dplyr::coalesce(x, FALSE))
+        dplyr::group_by_("pie.id") %>%
+        dplyr::mutate_at(dplyr::vars(), function(x) dplyr::coalesce(x, FALSE)) %>%
+        dplyr::ungroup()
 }

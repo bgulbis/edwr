34260b9aaba0f901ea5f25845607a3f7bf0ad620
bgulbis <brian.gulbis@gmail.com>
2016-07-09 14:04:13 -0500

    add summarize_data default and meds_cont methods

---
+++ R/summarize_data.R
@@ -0,0 +1,82 @@
+# summarize_data.R
+
+#' Summary calculations for continuous data
+#'
+#' \code{summarize_data} summarizes continuous data
+#'
+#' This function takes a data frame with continuous data (e.g., medications, lab
+#' results) and produces a data frame with summary data for each patient.
+#'
+#' For continuous medications, the calculations include: first rate, last rate,
+#' minimum rate, maximum rate, AUC, time-weighted average rate, total infusion
+#' duration, total infusion running time, and cumulative dose.
+#'
+#' @param x A data frame with continuous data
+#' @param units An optional character string specifying the time units to use in
+#'   calculations, default is hours
+#'
+#' @return A data frame
+#'
+#' @export
+summarize_data <- function(x, ...) {
+    UseMethod("summarize_data")
+}
+
+#' @export
+#' @rdname summarize_data
+summarize_data.default <- function(x, ...) {
+    warning(paste("No summarize_data method available for class", class(x)))
+    x
+}
+
+#' @export
+#' @rdname summarize_data
+#' @importFrom magrittr %>%
+summarize_data.meds_cont <- function(x, units = "hours", ...) {
+    # turn off scientific notation
+    options(scipen = 999)
+
+    cont <- dplyr::group_by_(x, .dots = list("pie.id",
+                                                  "med",
+                                                  "drip.count"))
+
+    # get last and min non-zero rate
+    nz.rate <- dplyr::filter_(cont, .dots = ~(med.rate > 0)) %>%
+        dplyr::summarize_(.dots = purrr::set_names(
+            x = list(~dplyr::last(med.rate),
+                     ~min(med.rate, na.rm = TRUE),
+                     ~sum(duration, na.rm = TRUE)),
+            nm = c("last.rate", "min.rate", "run.time")
+        ))
+
+    # get first and max rates and AUC
+    cont <- dplyr::summarize_(cont, .dots = purrr::set_names(
+        x = list(~dplyr::first(rate.start),
+                 ~dplyr::last(rate.stop),
+                 ~sum(med.rate * duration, na.rm = TRUE),
+                 ~dplyr::first(med.rate),
+                 ~max(med.rate, na.rm = TRUE),
+                 ~MESS::auc(run.time, med.rate),
+                 ~dplyr::last(run.time)),
+        nm = c("start.datetime", "stop.datetime", "cum.dose", "first.rate",
+               "max.rate", "auc", "duration")
+    )) %>%
+        # join the last and min data
+        dplyr::inner_join(nz.rate, by = c("pie.id", "med", "drip.count")) %>%
+        # calculate the time-weighted average and interval
+        dplyr::mutate_(.dots = purrr::set_names(
+            x = list(~auc/duration),
+            nm = "time.wt.avg"
+        ))
+
+    # keep original class
+    class(cont) <- class(x)
+    cont
+}
+
+#' @export
+#' @rdname summarize_data
+#' @importFrom magrittr %>%
+summarize_data.labs <- function(x, units = "hours", ...) {
+
+}

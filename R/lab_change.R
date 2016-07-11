f21397c2fdc6d7e25153ae80b6956841d9cb6727
bgulbis <brian.gulbis@gmail.com>
2016-07-11 13:43:06 -0500

    include lab in arguments to preserve S3 class

--- R/lab_change.R
+++ R/lab_change.R
@@ -11,6 +11,7 @@
 #' looking for an increase in lab value.
 #'
 #' @param x A data frame with lab data
+#' @param .lab A character string indicating the name of the lab to evaluate
 #' @param change.by A numeric indicating the threshold for lab changes
 #' @param FUN A function for \code{\link[zoo]{rollapplyr}}, most commonly max or
 #'   min
@@ -22,19 +23,20 @@
 #' @examples
 #' # checks for a >= 2 decrease in the hemoglobin value within the past 2 days
 #' x <- tidy_data(labs)
-#' x <- dplyr::filter(x, lab == "hgb")
 #'
 #' print(head(
-#'   lab_change(x, -2, max, back = 2)
+#'   lab_change(x, "hgb", -2, max, back = 2)
 #' ))
 #'
 #' @export
-lab_change <- function(x, change.by, FUN, back = 2) {
-    lab <- dplyr::group_by_(x, .dots = "pie.id") %>%
+lab_change <- function(x, .lab, change.by, FUN, back = 2) {
+    # calculate the number of rows that are included within the window, then
+    # calculate the running min/max during the time window, then calculate the
+    # change from the running min/max to current value, then filter values which
+    # exceed the change.by value
+    lab <- dplyr::filter_(x, .dots = list(~lab %in% .lab)) %>%
+        dplyr::group_by_(.dots = list("pie.id", "lab")) %>%
         dplyr::arrange_(.dots = "lab.datetime") %>%
-        # calculate the number of rows that are included within the window, then
-        # calculate the running min/max during the time window, then calculate
-        # the change from the running min/max to current value
         dplyr::mutate_(.dots = purrr::set_names(
             x = list(~count_rowsback(lab.datetime, back),
                      ~zoo::rollapplyr(lab.result, rowsback, FUN, fill = NA,
@@ -42,7 +44,6 @@ lab_change <- function(x, change.by, FUN, back = 2) {
                      ~lab.result - running),
             nm = list("rowsback", "running", "change")
         )) %>%
-        # filter values which exceed the change.by value
         dplyr::filter_(.dots = list(~abs(change) >= abs(change.by)))
 
     # keep original class

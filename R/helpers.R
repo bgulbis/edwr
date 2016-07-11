22434f444a52ebea5b12885f447c5dcb03d99f47
bgulbis <brian.gulbis@gmail.com>
2016-07-11 13:48:12 -0500

    code formatting

--- R/helpers.R
+++ R/helpers.R
@@ -3,9 +3,10 @@
 # make sure all patients are included in the data frame and the values for added
 # patients are FALSE
 add_patients <- function(tidy, patients) {
-    tidy <- dplyr::full_join(tidy, patients["pie.id"], by = "pie.id") %>%
+    dplyr::full_join(tidy, patients["pie.id"], by = "pie.id") %>%
         dplyr::group_by_("pie.id") %>%
-        dplyr::mutate_at(dplyr::vars(), function(x) dplyr::coalesce(x, FALSE)) %>%
+        dplyr::mutate_at(dplyr::vars(),
+                         function(x) dplyr::coalesce(x, FALSE)) %>%
         dplyr::ungroup()
 }
 

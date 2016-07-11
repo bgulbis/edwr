4a6075122ad85510bb731c2ceae0c076414262c6
bgulbis <brian.gulbis@gmail.com>
2016-07-11 12:42:17 -0500

    add count_rowsback helper

--- R/helpers.R
+++ R/helpers.R
@@ -8,3 +8,16 @@ add_patients <- function(tidy, patients) {
         dplyr::mutate_at(dplyr::vars(), function(x) dplyr::coalesce(x, FALSE)) %>%
         dplyr::ungroup()
 }
+
+
+# Count the number of rows to go back in data frame. Takes a vector of POSIXct
+# and counts the number of rows which would fall within the specified time
+# frame. Typically called from dplyr::mutate and the results are passed on to
+# zoo::rollapplyr.
+count_rowsback <- function(x, back = 2) {
+    curr_val <- function(y) {
+        sum(x >= y - lubridate::days(back) & x <= y)
+    }
+
+    purrr::map_int(x, curr_val)
+}

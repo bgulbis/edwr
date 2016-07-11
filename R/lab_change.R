ba73db3b87e0710753e0e5c1d3ad31a23f13b3cc
bgulbis <brian.gulbis@gmail.com>
2016-07-11 12:42:34 -0500

    add lab_change function

---
+++ R/lab_change.R
@@ -0,0 +1,46 @@
+# lab_change.R
+
+#' Determine if a lab changed by a set amount within a specific time frame
+#'
+#' \code{lab_change} checks for changes in lab values
+#'
+#' This function takes a data frame with lab data for a single lab and checks
+#' whether the lab changes by a certain amount within a given period of time.
+#'
+#' For FUN, use max when looking for a decrease in lab value, and min when
+#' looking for an increase in lab value.
+#'
+#' @param x A data frame with lab data
+#' @param change.by A numeric indicating the threshold for lab changes
+#' @param FUN A function for \code{\link[zoo]{rollapplyr}}, most commonly max or
+#'   min
+#' @param back An optional numeric specifying the number of days back to go.
+#'   Defaults to 2 days.
+#'
+#' @return A data frame
+#'
+#' @examples
+#' # checks for a >= 2 decrease in the lab value within the past 2 days
+#' x <- tidy_data(labs)
+#' y <- lab_change(x, -2, max, back = 2)
+#'
+#' @export
+lab_change <- function(x, change.by, FUN, back = 2) {
+    lab <- dplyr::group_by_(x, .dots = "pie.id") %>%
+        dplyr::arrange_(.dots = "lab.datetime") %>%
+        # calculate the number of rows that are included within the window, then
+        # calculate the running min/max during the time window, then calculate
+        # the change from the running min/max to current value
+        dplyr::mutate_(.dots = purrr::set_names(
+            x = list(~count_rowsback(lab.datetime, back),
+                     ~zoo::rollapplyr(lab.result, rowsback, FUN, fill = NA, partial = TRUE),
+                     ~lab.result - running),
+            nm = list("rowsback", "running", "change")
+        )) %>%
+        # filter values which exceed the change.by value
+        dplyr::filter_(.dots = list(~abs(change) >= abs(change.by)))
+
+    # keep original class
+    class(lab) <- class(x)
+    lab
+}

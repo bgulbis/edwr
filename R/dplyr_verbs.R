2128e42b0963a17877ce4b65e2a707a436923265
bgulbis <brian.gulbis@gmail.com>
2016-07-13 20:09:37 -0500

    additional dplyr verbs and joins

--- R/dplyr_verbs.R
+++ R/dplyr_verbs.R
@@ -1,6 +1,15 @@
 # dplyr_verbs.R
 
-# create generic functions for dplyr verbs which maintain edwr class types
+# create generic functions for dplyr verbs and joins which maintain edwr class
+# types; for use in scripts and interactive programming only, use in functions
+# should still call dplyr::_ (SE) functions
+
+#' @export
+arrange.edwr <- function(.data, ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
 
 #' @export
 filter.edwr <- function(.data, ...) {
@@ -10,7 +19,51 @@ filter.edwr <- function(.data, ...) {
 }
 
 #' @export
-filter_.edwr <- function(.data, ..., .dots) {
+mutate.edwr <- function(.data, ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+select.edwr <- function(.data, ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+summarize.edwr <- function(.data, ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+anti_join.edwr <- function(x, y, by = NULL, copy = FALSE, ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+full_join.edwr <- function(x, y, by = NULL, copy = FALSE,
+                           suffix = c(".x", ".y"), ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+inner_join.edwr <- function(x, y, by = NULL, copy = FALSE,
+                            suffix = c(".x", ".y"), ...) {
+    y <- NextMethod()
+    class(y) <- class(.data)
+    y
+}
+
+#' @export
+semi_join.edwr <- function(x, y, by = NULL, copy = FALSE, ...) {
     y <- NextMethod()
     class(y) <- class(.data)
     y

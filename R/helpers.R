8b9a2004b7cc9512e300a155a63bf4f2d23708de
bgulbis <brian.gulbis@gmail.com>
2016-07-15 12:33:09 -0500

    move format_dates to helper file; add internal documentation for count_rowsback

--- R/helpers.R
+++ R/helpers.R
@@ -11,14 +11,41 @@ add_patients <- function(tidy, patients) {
 }
 
 
-# Count the number of rows to go back in data frame. Takes a vector of POSIXct
-# and counts the number of rows which would fall within the specified time
-# frame. Typically called from dplyr::mutate and the results are passed on to
-# zoo::rollapplyr.
+#' Count the number of rows to go back in data frame
+#'
+#' Takes a vector of POSIXct and counts the number of rows which would fall
+#' within the specified time frame. Typically called from
+#' \code{\link[dplyr]{mutate}} and the results are passed on to
+#' \code{\link[zoo]{rollapplyr}}.
+#'
+#' @param x = vector of type POSIXct
+#' @param back = integer indicating the number of days back to include
+#'
+#' @return integer vector
+#'
+#' @keywords internal
 count_rowsback <- function(x, back = 2) {
     curr_val <- function(y) {
         sum(x >= y - lubridate::days(back) & x <= y)
     }
 
     purrr::map_int(x, curr_val)
+}
+
+#' Set the default format for reading date/time variables
+#'
+#' Set the default format for parsing date/time variables when creating
+#' \code{edwr} classes.
+#'
+#' @param x character vector of date/time data
+#'
+#' @return A \code{\link[readr]{collector}} object
+#'
+#' @keywords internal
+format_dates <- function(x) {
+    readr::parse_datetime(
+        x = x,
+        format = "%Y/%m/%d %H:%M:%S",
+        locale = readr::locale(tz = "US/Central")
+    )
 }

30c386dc668f32c1b3f88b3d73604bf47df0755d
bgulbis <brian.gulbis@gmail.com>
2016-07-16 23:54:26 -0500

    additional dplyr verbs

--- R/dplyr_verbs.R
+++ R/dplyr_verbs.R
@@ -5,72 +5,81 @@
 #' @export
 arrange_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    keep_class(.data, y)
 }
 
 #' @export
 filter_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    keep_class(.data, y)
 }
 
 #' @export
 mutate_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    print(class(.data))
+    print(class(y))
+    keep_class(.data, y)
 }
 
 #' @export
 rename_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    keep_class(.data, y)
 }
-
 
 #' @export
 select_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    keep_class(.data, y)
 }
 
 #' @export
 summarise_.edwr <- function(.data, ..., .dots) {
     y <- NextMethod()
-    class(y) <- class(.data)
-    y
+    keep_class(.data, y)
+}
+
+#' @export
+group_by_.edwr <- function(.data, ..., .dots, add = FALSE) {
+    y <- NextMethod()
+    keep_class(.data, y)
+}
+
+#' @export
+ungroup.edwr <- function(x, ...) {
+    y <- NextMethod()
+    keep_class(x, y)
+}
+
+#' @export
+distinct_.edwr <- function(.data, ..., .dots, .keep_all = FALSE) {
+    y <- NextMethod()
+    keep_class(.data, y)
 }
 
 #' @export
 anti_join.edwr <- function(x, y, by = NULL, copy = FALSE, ...) {
     z <- NextMethod()
-    class(z) <- class(x)
-    z
+    keep_class(x, z)
 }
 
 #' @export
 full_join.edwr <- function(x, y, by = NULL, copy = FALSE,
                            suffix = c(".x", ".y"), ...) {
     z <- NextMethod()
-    class(z) <- class(x)
-    z
+    keep_class(x, z)
 }
 
 #' @export
 inner_join.edwr <- function(x, y, by = NULL, copy = FALSE,
                             suffix = c(".x", ".y"), ...) {
     z <- NextMethod()
-    class(z) <- class(x)
-    z
+    keep_class(x, z)
 }
 
 #' @export
 semi_join.edwr <- function(x, y, by = NULL, copy = FALSE, ...) {
     z <- NextMethod()
-    class(z) <- class(x)
-    z
+    keep_class(x, z)
 }

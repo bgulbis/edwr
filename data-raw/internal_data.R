cc20db1600c1dd62a40234a72df1bb6b4d203a9d
Brian Gulbis <brian.gulbis@gmail.com>
2016-08-18 20:39:05 -0500

    add list of pregnancy icd codes to internal data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -6,6 +6,7 @@
 # last updated: 6/30/2016
 
 library(dplyr)
+library(icd)
 
 dirr::gzip_files("data-raw", pattern = "medication_classes.csv")
 
@@ -14,4 +15,16 @@ med.classes <- readr::read_csv("data-raw/medication_classes.csv.gz") %>%
            med.name = `Generic Drug Name`) %>%
     distinct
 
-devtools::use_data(med.classes, internal = TRUE, overwrite = TRUE)
+icd9 <- stringr::str_detect(names(icd::icd9_chapters), "Pregnancy")
+preg.icd9 <- list(pregnant = icd::icd_expand_range(
+    start = icd::icd9_chapters[icd9][[1]][[1]],
+    end = icd::icd9_chapters[icd9][[1]][[2]]))
+
+icd10 <- stringr::str_detect(names(icd::icd10_chapters), "Pregnancy")
+preg.icd10 <- list(pregnant = icd::icd_expand_range(
+    start = icd::icd10_chapters[icd10][[1]][[1]],
+    end = icd::icd10_chapters[icd10][[1]][[2]]))
+
+devtools::use_data(med.classes, preg.icd9, preg.icd10,
+                   internal = TRUE,
+                   overwrite = TRUE)

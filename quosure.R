94e08bc17afe2b3f4e96bfb841b37b9139fa4441
bgulbis <brian.gulbis@gmail.com>
2018-01-22 15:58:26 -0600

    test quo

--- quosure.R
+++ quosure.R
@@ -1,14 +1,27 @@
 library(tidyverse)
 library(edwr)
+library(rlang)
 
 attr(meds_sched, "data") <- "edw"
 x <- meds_sched %>%
     mutate_at("med.dose", as.numeric) %>%
-    calc_runtime()
-y <- summarize_data(x)
+    calc_runtime() %>%
+    summarize_data()
 
 attr(labs, "data") <- "edw"
 z <- labs %>%
     tidy_data() %>%
     calc_runtime() %>%
     summarize_data()
+
+attr(meds_cont, "data") <- "edw"
+y <- meds_cont %>%
+    mutate_at("med.rate", as.numeric) %>%
+    calc_runtime() %>%
+    summarize_data()
+
+id <- quo(!!sym("id"))
+grp_by <- quos(!!id, !!sym("med"), !!sym("drip.count"))
+
+quo_text(id)
+c(quo_text(id), "med", "drip.count")

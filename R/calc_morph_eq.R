71574355461ebf629ebee2ea50ab9c460eecb820
bgulbis <brian.gulbis@gmail.com>
2018-02-14 11:08:43 -0600

    working morphine equiv function

--- R/calc_morph_eq.R
+++ R/calc_morph_eq.R
@@ -7,22 +7,48 @@
 #' @param x data_frame
 #'
 #' @return data_frame
+#'
+#' @references Lexicomp - Opiod Agonist Conversion (from drug, to Morphine IM);
+#' https://www.cms.gov/Medicare/Prescription-Drug-Coverage/PrescriptionDrugCovContra/Downloads/Opioid-Morphine-EQ-Conversion-Factors-March-2015.pdf (note, doses listed in oral mme, converted to iv mme by * 0.3);
+#' http://clincalc.com/opioids/
+#'
 #' @export
 calc_morph_eq <- function(x) {
     # med.dose <- enquo(med.dose)
 
+    med <- sym("med")
+    route.group <- sym("route.group")
+    dose.mg <- sym("dose.mg")
+    med.dose.units <- sym("med.dose.units")
+
+    convert <- quos(med == "buprenorphine" & route.group == "TOP" ~ 3.8,
+                    med == "buprenorphine" & route.group == "PO" ~ 3,
+                    med == "butorphenol" ~ dose.mg * 5,
+                    stringr::str_detect(med, "codeine") & route.group == "PO" ~ dose.mg * 0.1,
+                    med == "fentanyl" & route.group == "IV" ~ dose.mg * 0.1,
+                    med == "fentanyl" & route.group == "NASAL" ~ dose.mg * 0.16 * 0.3,
+                    med == "fentanyl" & route.group == "TOP" ~ dose.mg * 7.2 * 0.3,
+                    stringr::str_detect(med, "hydrocodone") ~ dose.mg * 1 * 0.3,
+                    med == "hydromorphone" & route.group == "PO" ~ dose.mg * 1.3,
+                    med == "hydromorphone" & route.group == "IV" ~ dose.mg * 6.7,
+                    med == "levorphanol" ~ dose.mg * 5,
+                    med == "mepridine" ~ dose.mg * 0.1,
+                    med == "methadone" & route.group == "PO" ~ dose.mg * 3 * 0.3,
+                    med == "morphine" & route.group == "PO" ~ dose.mg * 0.3,
+                    med == "nalbuphine" ~ dose.mg * 1,
+                    med == "opium" ~ dose.mg * 1 * 0.3,
+                    stringr::str_detect(med, "oxycodone") & route.group == "PO" ~ dose.mg * 0.5,
+                    med == "oxymorphone" & route.group == "PO" ~ dose.mg * 1,
+                    med == "oxymorphone" & route.group == "IV" ~ dose.mg * 10,
+                    med == "pentazocine" ~ dose.mg * 0.37 * 0.3,
+                    med == "tapentadol" ~ dose.mg * 0.1,
+                    med == "remifentanil" & med.dose.units == "mg" ~ dose.mg * 100, # dose recorded in mg; assume equiv with fentanyl
+                    med == "sufentanil" ~ dose.mg * 0.5, # 100mcg = 50mg
+                    med == "tramadol" ~ dose.mg * 0.1 * 0.3,
+                    TRUE ~ dose.mg)
+
     df <- x %>%
-        mutate(!!"morph_eq" := !!parse_expr('dplyr::case_when(
-            med == "hydromorphone" & (route == "IV" | route == "IVP") ~ med.dose * 0.4,
-            med == "hydromorphone" & (route == "PO" | route == "NG") ~ med.dose / 0.8,
-            med == "fentanyl" & (route == "IV" | route == "IVP") ~ med.dose * 0.1,
-            med == "oxycodone" & (route == "PO" | route == "NG") ~ med.dose / 2.5,
-            med == "acetaminophen-hydrocodone" & med.dose.units == "mL" ~ med.dose * 10 / 15 / 2.5,
-            med == "acetaminophen-hydrocodone" & med.dose.units == "mg" ~ med.dose / 2.5,
-            med == "acetaminophen-codeine" & med.dose.units == "mL" ~ med.dose * 12 / 5 / 6,
-            med == "acetaminophen-codeine" & med.dose.units == "mg" ~ med.dose / 6,
-            med == "tramadol" & (route == "PO" | route == "NG") ~ med.dose / 25,
-            TRUE ~ med.dose)'))
+        mutate(!!"mme.iv" := dplyr::case_when(!!!convert))
 
     reclass(x, df)
 }
